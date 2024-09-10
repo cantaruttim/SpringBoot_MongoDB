ab90fed2c58231273df5b4ccddb4b337b883488a
Matheus Cantarutti <cantaruttim@outlook.com>
2024-09-09 22:47:05 -0300

    Consulta simples com Query Methods

--- src/main/java/com/cantaruttimatheus/SpringBoot/repository/PostRepository.java
+++ src/main/java/com/cantaruttimatheus/SpringBoot/repository/PostRepository.java
@@ -5,9 +5,13 @@ import com.cantaruttimatheus.SpringBoot.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // acesso à dados. Objetos que são responsáveis por efetuar operações básicas de acesso à dados.
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
     // 2) save, recupera, atualiza os dados ==> IR PARA SERVIÇOS
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/cantaruttimatheus/SpringBoot/resource/PostResource.java
+++ src/main/java/com/cantaruttimatheus/SpringBoot/resource/PostResource.java
@@ -3,7 +3,9 @@ package com.cantaruttimatheus.SpringBoot.resource;
 import com.cantaruttimatheus.SpringBoot.domain.Post;
 import com.cantaruttimatheus.SpringBoot.domain.User;
 import com.cantaruttimatheus.SpringBoot.dto.UserDTO;
+import com.cantaruttimatheus.SpringBoot.resource.util.URL;
 import com.cantaruttimatheus.SpringBoot.service.PostService;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,4 +27,13 @@ public class PostResource {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>>  findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.finByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
